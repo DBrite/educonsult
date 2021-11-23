f45ab32e0c5499ac3e179bd223b378b97b13860d
DBrite <paragonbright1999@gmail.com>
2021-11-23 00:08:02 +0000

    update

--- educonsult_app/models.py
+++ educonsult_app/models.py
@@ -4,7 +4,7 @@ from django.db.models import constraints
 # Create your models here.
 
 class Contact(models.Model):
-    contact_id = models.AutoField(primary_key=True)
+    id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=50)
     email = models.EmailField()
     phone = models.CharField(max_length=10)
